043f5ad31abbb00ae7c291a776fd6a16ece9d945
Ольга <koval.oi.1106@gmail.com>
2024-04-15 20:17:56 +0500

    ДЗ 3.1

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,51 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Task1");
+    int age = 19;
+    if (age >= 18){
+        System.out.println("ты совершеннолетний");
+    }
+    else {
+        System.out.println(" нужно еще подождать до совершеннолетия");
+    }
+        System.out.println("Task2");
+    int temperature = -7;
+    if (temperature <= 5) {
+        System.out.println("На улице холодно, нужно надеть шапку");}
+    else {
+        System.out.println("Сегодня тепло, можно идти без шапки");
+    }
+        System.out.println("Task3");
+    int speed = 90;
+    if (speed <=60) {
+        System.out.println("Скорость в рамках допустимого, можно ездить спокойно");
+    }
+    else {
+        System.out.println("Скорость превышена, нужно оплатить штраф");
+    }
+        System.out.println("Task4");
+    int yearsOld = 8;
+    if (yearsOld >= 2 && yearsOld <=6) {
+        System.out.println("Нужно идти в детский сад");}
+    if (yearsOld >=7 && yearsOld <= 17) {
+            System.out.println("нужно ходить в школу");}
+    if (yearsOld >= 18 && yearsOld <= 24) {
+        System.out.println("место в университете");}
+        if (yearsOld >=24) {
+            System.out.println("пора ходить на работу");}
+        System.out.println("Task5");
+        int childAge = 29;
+        if (childAge <=5) {
+            System.out.println("нельзя кататься на аттракционе");
+        }
+        if (childAge >=5 && childAge<=14) {
+            System.out.println("можно кататься на аттракционе в сопровождении");
+        }
+        if (age == 14 || childAge >=14) {
+            System.out.println("без сопровождения взрослого");
+        }
         }
     }
-}
+
+
